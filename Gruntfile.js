7b968008b443c88759d0b5d270c9bf3524cf3600
ljfranklin <lylejfranklin@gmail.com>
2014-05-16 18:35:25 -0700

    Cleaned up jshint task

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,28 +7,7 @@ module.exports = function (grunt) {
             files: ['Gruntfile.js', 'app/scripts/**/*.js', 'test/e2e/*.js', 'test/unit/**/*.js', 'test/*.js'],
             options: {
                 ignores: ['app/bower_components/', 'test/bower_components/', 'node_modules/'],
-                strict: true,
-                browser: true,
-                node: true,
-                globals: {
-                    /* mocha */
-                    describe: false,
-                    it: false,
-                    before: false,
-                    beforeEach: false,
-                    after: false,
-                    afterEach: false,
-                    /* requirejs */
-                    require: false,
-                    define: false,
-                    /* angularjs */
-                    inject: false,
-                    /* protractor */
-                    protractor: false,
-                    browser: false,
-                    by: false,
-                    element: false
-                }
+                jshintrc: true
             }
         }
     });
