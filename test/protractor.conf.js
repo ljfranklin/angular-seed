5de1b47e6683be363cdf5e9d7ecee6a82a611e52
ljfranklin <lylejfranklin@gmail.com>
2014-05-21 09:49:03 -0700

    Added saucelabs config for travis

--- test/protractor.conf.js
+++ test/protractor.conf.js
@@ -2,12 +2,18 @@
 
 exports.config = {
 
+    sauceUser: process.env.SAUCE_USERNAME,
+    sauceKey: process.env.SAUCE_ACCESS_KEY,
+
     specs: [
         'e2e/*.js'
     ],
 
     capabilities: {
-        'browserName': 'chrome'
+        'browserName': 'chrome',
+        'name': 'Angular Seed Stack Protractor Tests',
+        'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+        'build': process.env.TRAVIS_BUILD_NUMBER,
     },
 
     baseUrl: 'http://localhost:8001/',
